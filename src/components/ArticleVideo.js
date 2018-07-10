890b71eac1aa3541101a7a0e197be1e470749a54
Zack Vera <qphys50@gmail.com>
2018-07-10 16:00:05 -0600

    Added the title caption to images and the video

--- src/components/ArticleVideo.js
+++ src/components/ArticleVideo.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import YouTube from 'react-youtube';
 
 class ArticleVideo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videoTitle: '',
+    };
+  }
+
   render() {
     const opts = {
       width: 320,
       height: 180,
+    };
+
+    const getTitle = (event) => {
+      const player = event.target;
+      const data = player.getVideoData();
+      this.setState({ videoTitle: data.title });
     };
 
     return (
@@ -13,7 +27,14 @@ class ArticleVideo extends Component {
         <YouTube
           videoId="fCBusmHtFuU"
           opts={opts}
+          onReady={getTitle}
         />
+        <h4>
+          {this.state.videoTitle}
+        </h4>
+        <p>
+          Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        </p>
       </div>
     );
   }
